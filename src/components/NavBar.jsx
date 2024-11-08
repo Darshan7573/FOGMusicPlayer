2a76f4d780de32bc1459485245c56ec26c5d0877
Darshan7573 <darshu7573@gmail.com>
2024-11-08 19:47:10 +0530

    NavBar component is done

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import { IoMdSearch } from "react-icons/io";
+
+const NavBar = () => {
+    return (
+        <div className="items-center p-[50px] ml-[100px]">
+            <div className="flex flex-row  h-[27px] top-[45px] left-[519px] gap-[50px]">
+                <ul className="flex flex-row gap-6 text-sm text-white cursor-pointer">
+                    <li>Music</li>
+                    <li>Podcast</li>
+                    <li>Live</li>
+                    <li>Radio</li>
+                </ul>
+                <div className="relative ml-[100px] flex items-center">
+                    <input
+                        className="border-none text-[18px] bg-[#200101] text-white  rounded-[50px] w-[400px] top-[30px] left-[964px] h-[38px] focus:outline-none pl-[20px]"
+                        type="text"
+                    />
+                    <IoMdSearch className="absolute text-2xl right-3 top-1/2 transform -translate-y-1/2 text-white" />
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,21 @@
+// import Banner from "../components/Banner"
+import NavBar from "../components/NavBar"
 import SideBar from "../components/SideBar"
+// import SongList from "../components/SongList"
 
 const HomePage = () => {
     return (
-        <div>
-            <SideBar />
-        </div>
+        <>
+            <div className="flex h-screen">
+                <SideBar />
+                <div className="flex-1 bg-gradient-to-t from-[#0a0303] to-[#7E0301]">
+                    <NavBar />
+                    {/* <Banner />
+                    <SongList /> */}
+                </div>
+            </div>
+        </>
+
     )
 }
 
